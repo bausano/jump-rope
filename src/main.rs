9bc2ca8eddc4cbe89e43ec74a56df1f402470065
porkbrain <git@porkbrain.com>
2021-09-03 12:03:26 +0100

    Shared state sent to bevy UI via one shot channel

--- src/main.rs
+++ src/main.rs
@@ -10,65 +10,77 @@ mod ui;
 use crate::analyzer::AnalyzerBuilder;
 use crate::frame::FrameIter;
 use frequency_tracker::FrequencyTracker;
+use std::sync::mpsc::channel;
 use std::sync::Arc;
 use std::thread;
 
 fn main() {
     ffmpeg::init().unwrap();
 
-    // TODO: don't hard code FPS but get it from camera settings
-    let frequency_tracker = Arc::new(FrequencyTracker::new(10));
+    let frequency_tracker = start_video_analysis();
 
-    let frequency_tracker_clone = Arc::clone(&frequency_tracker);
-    thread::spawn(move || start_video_analysis(frequency_tracker_clone));
-
+    // bevy must always run on main thread
     ui::start(frequency_tracker);
 }
 
 // Starts iterating the video frames with various window sizes and updates the
 // tracker with latest values.
 //
-// # Important
-// This method blocks until video stops.
-fn start_video_analysis(frequency_tracker: Arc<FrequencyTracker>) {
-    // TODO: camera might not be on this device
-    let file = "/dev/video0";
-    let frames = FrameIter::from_file(file).expect("Cannot load video");
-    let frame_rate = frames.frame_rate();
-    println!("FPS: {}", frame_rate);
+// Returns a shared state abstraction to read the latest frequency.
+fn start_video_analysis() -> Arc<FrequencyTracker> {
+    // creates new one shot channel to send shared state reference because:
+    // 1. bevy must run on the main thread
+    // 2. [`FrameIter`] cannot be shared between threads safely after
+    //    initialization
+    let (sender, receiver) = channel();
+    thread::spawn(move || {
+        // TODO: camera might not be on this device
+        let file = "/dev/video0";
+        let frames = FrameIter::from_file(file).expect("Cannot load video");
+        let frame_rate = frames.frame_rate();
+        println!("FPS: {}", frame_rate);
 
-    // The larger the multiplier, the more granular frequency intervals it can
-    // find. However, it takes longer to start reporting and it takes longer to
-    // adjust to rapid speed changes.
-    //
-    // We therefore spawn multiple and let them reach a consensus.
-    const WINDOW_MULTIPLIERS: &[usize] = &[4, 8, 12];
-    let channels: Vec<_> = WINDOW_MULTIPLIERS
-        .iter()
-        .map(|multiplier| {
-            analyzer::channel(AnalyzerBuilder {
-                frame_rate,
-                window: frame_rate * *multiplier,
-                frame_height: frames.height(),
-                frame_width: frames.width(),
+        // create shared state abstraction and send a clone reference to
+        // the main thread
+        let frequency_tracker = Arc::new(FrequencyTracker::new(frame_rate));
+        sender.send(Arc::clone(&frequency_tracker)).unwrap();
+
+        // The larger the multiplier, the more granular frequency intervals it
+        // can find. However, it takes longer to start reporting and it takes
+        // longer to adjust to rapid speed changes.
+        //
+        // We therefore spawn multiple and let them reach a consensus.
+        const WINDOW_MULTIPLIERS: &[usize] = &[4, 8, 12];
+        let channels: Vec<_> = WINDOW_MULTIPLIERS
+            .iter()
+            .map(|multiplier| {
+                analyzer::channel(AnalyzerBuilder {
+                    frame_rate,
+                    window: frame_rate * *multiplier,
+                    frame_height: frames.height(),
+                    frame_width: frames.width(),
+                })
             })
-        })
-        .collect();
+            .collect();
 
-    for frame in frames {
-        // update each analyzer (they differ by window) with the new frame
-        let frame = Arc::new(frame);
-        channels.iter().for_each(|(frame_sender, _)| {
-            frame_sender.send(Arc::clone(&frame)).expect("Channel dead")
-        });
+        for frame in frames {
+            // update each analyzer (they differ by window) with the new frame
+            let frame = Arc::new(frame);
+            channels.iter().for_each(|(frame_sender, _)| {
+                frame_sender.send(Arc::clone(&frame)).expect("Channel dead")
+            });
 
-        // TODO: if no update for long time, clean tracker
-        // check for frequency updates
-        for (_, frequency_recv) in &channels {
-            // we only care about the freshest value
-            if let Some(report) = frequency_recv.try_iter().last() {
-                frequency_tracker.update(report);
+            // TODO: if no update for long time, clean the tracker
+            //
+            // check for frequency updates
+            for (_, frequency_recv) in &channels {
+                // we only care about the freshest value
+                if let Some(report) = frequency_recv.try_iter().last() {
+                    frequency_tracker.update(report);
+                }
             }
         }
-    }
+    });
+
+    receiver.recv().unwrap()
 }
